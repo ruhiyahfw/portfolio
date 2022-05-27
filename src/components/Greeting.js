16c230952d2f9a3b2b39555ed2c264fc157a5f56
Ruhiyahfw <faradishi@gmail.com>
2022-05-27 11:47:46 +0700

    change a bit

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -10,7 +10,7 @@ function Greeting(){
                     <img id='profpic' src={profpic} alt='profpic'/>
                 </div>
                 <div className='right-grid-item'>
-                    <h1 className='highlight-text'>Hello!</h1>
+                    <h1 className='highlight-text'>Hellow!</h1>
                     <h1>I am <span className='color1'>Ruhiyah Faradishi Widiaputri</span></h1>
                     <p>a third year computer science student who loves to learn and grow</p>
                     <p className="ket">Nice to meet you ^^</p>
